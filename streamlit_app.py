5512cfd1cc373e753da61d0325bc351c7aa4058b
Suraj <83573550+suraj-4747@users.noreply.github.com>
2025-04-29 20:45:13 +0530

    Update streamlit_app.py
    removed import session and added line at 12 and modified line 13

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -10,7 +9,8 @@ st.write("Choose the fruits you want in smoothie:")
 name_on_order = st.text_input('Name of smoothie:')
 st.write('The name of smoothie will be:', name_on_order)
 
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect(
